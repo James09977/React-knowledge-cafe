5973f94fa17002610aafd2b08ff0c4e5ead3d249
James09977 <zakaria.james1@gmail.com>
2024-11-15 20:05:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-cyan-800 bg-orange-400">
+        Knowledge cafe
+      </h1>
+    </>
+  );
+}
+
+export default App;
